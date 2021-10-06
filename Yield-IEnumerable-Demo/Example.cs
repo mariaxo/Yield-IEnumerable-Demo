d7f97039afc8790cb99c88289ed78ccd85ab5016
Maria Abrahamyan <maria.abrahamyan@dataart.com>
2021-10-06 19:13:17 +0400

    Add project files.

---
+++ Yield-IEnumerable-Demo/Example.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YieldAndEnumerable
+{
+    class T
+    {
+        //returns a list of type IEnumerable<>
+
+        //iterator
+        public IEnumerable<int> GetT(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                if (item > 5)
+                    yield return item;
+            }
+        }
+
+        //a standard method
+        public IEnumerable<int> GetT1(List<int> list)
+        {
+            List<int> ttt = new List<int>();
+            foreach (var item in list)
+            {
+                if (item > 5)
+                    ttt.Add(item);
+            }
+            return ttt;
+        }
+    }
+
+    class Try
+    {
+        static void M1()
+        {
+            var t = new T();
+            var list = new List<int> { 2, 5, 6, 1, 11 };
+
+            //evaluates just the first element, just iterates once
+            var result = t.GetT(list).FirstOrDefault();
+            var result0 = t.GetT(list);
+            Console.WriteLine(string.Join(" ", result));
+
+            //evaluates the whole list, then returns (iterates over the full list)
+            var result1 = t.GetT1(list).FirstOrDefault();
+        }
+    }
+}
---
+++ Yield-IEnumerable-Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Yield_IEnumerable_Demo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GetList().FirstOrDefault(e => e > 3);
+
+			GetListYield().FirstOrDefault(e => e > 3);
+		}
+
+		static IEnumerable<int> GetListYield()
+		{
+			var list = new List<int> { 2, 4, 2, 1, 3 };
+
+			foreach (var element in list)
+			{
+				yield return element;
+			}
+		}
+
+		static IEnumerable<int> GetList()
+		{
+			var list = new List<int> { 2, 4, 2, 1, 3 };
+			var newList = new List<int>();
+
+			foreach (var element in list)
+			{
+				newList.Add(element);
+			}
+
+			return newList;
+		}
+	}
+}
